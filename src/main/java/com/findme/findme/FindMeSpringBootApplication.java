2fb2368084e96d678b921abc0442ab061402ef7c
Illia <1gtagat@gmail.com>
2022-07-12 00:04:08 +0300

    add pom.xml and FindMeSpringBootApplication.java

---
+++ src/main/java/com/findme/findme/FindMeSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.findme.findme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMeSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindMeSpringBootApplication.class, args);
+    }
+
+}
